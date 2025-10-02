a0e89638d73c57fd2d3ebd01d6592c2cf8c48fb2
Mallow96 <mencheliu@gmail.com>
2025-10-02 15:03:28 +0800

    rest page

--- project foodie/src/pages/restaurant_page.vue
+++ project foodie/src/pages/restaurant_page.vue
@@ -11,12 +11,58 @@ const restaurant = restStore.restaurants.find((r) => String(r.id) === id);
 </script>
 
 <template>
-  <div v-if="restaurant">
-    <h2>{{ restaurant.name }}</h2>
-    <p>{{ restaurant.description }}</p>
-    <!-- 依據格式繼續展開其他欄位 -->
+  <div v-if="restaurant" class="title">
+    <div class="rest-head">
+      <h2>{{ restaurant.name }}</h2>
+      <h3 class="rating">{{ restaurant.rating }} / 5.0</h3>
+      <p class="review-count">({{ restaurant.reviewCount }})</p>
+      <h5 class="price-range">TWD ${{ restaurant.priceRange }} / 每人</h5>
+    </div>
+    <div class="contact">
+      <h5>
+        <i class="fa-solid fa-location-dot"></i>
+        {{ restaurant.address }}
+      </h5>
+      <h5>
+        <i class="fa-solid fa-phone"></i>
+        {{ restaurant.contactPhone }}
+      </h5>
+    </div>
   </div>
+
+  <!-- 錯誤 -->
   <div v-else>
-    <p>找不到該餐廳。</p>
+    <h2 class="text-center">找不到該餐廳。</h2>
   </div>
+  <!-- 錯誤 -->
 </template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.rest-head,
+.contact {
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+}
+
+.rest-head {
+  margin-bottom: 2.5rem;
+}
+
+.rating {
+  color: var(--color-primary-dbrown);
+}
+
+.review-count {
+  color: var(--color-primary-brown);
+}
+
+.price-range {
+  color: var(--color-brown-200);
+}
+</style>
