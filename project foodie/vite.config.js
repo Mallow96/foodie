9be7ee13245cb35947700eac0b2af046217b394d
Mallow96 <mencheliu@gmail.com>
2025-10-13 09:12:06 +0800

    build 1

--- project foodie/vite.config.js
+++ project foodie/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/foodie/", // 把 <repo-name> 換成你的專案名稱
+
   plugins: [vue()],
-})
+});
